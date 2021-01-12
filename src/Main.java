eaeca61e68b0503ff489d6f2915f0777344b23e9
isaccanedo <isaccanedo@gmail.com>
2021-01-12 12:33:01 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+
+public class Main {
+
+	/*
+	 * Which is the most reliable expression for testing whether the values of two string variables are the same?
+	 * Qual é a expressão mais confiável para testar se os valores de duas variáveis de string são iguais?
+	 * 
+	 * 1 - nome.matches(nome1) ==> correct
+	 * 2 - nome = nome1
+	 * 3 - nome == nome1
+	 * 4 - nome.equals(nome1)
+	 */
+	
+	
+	static String nome = "Isac";
+	static String nome1 = "isac";
+	
+	public static void main(String[] args) {
+		
+		System.out.println(nome.matches(nome1)); // sim
+		System.out.println(nome = nome1); // não
+		System.out.println(nome == nome1); // não
+		System.out.println(nome.equals(nome1)); // não
+
+		
+		
+	}
+
+}
